e2a2a89e41bfe69de43e306352e249920e255885
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-04 00:22:16 +0900

    [feat] MbtiHeader 95% 완성, 내일 계절 일찍 일어나야 해서 퇴근

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,7 +1,12 @@
 import AppLayout from '../../components/AppLayOut';
+import MbtiHeader from '../../components/MbtiHeader';
 
 const Profile = () => {
-  return <AppLayout></AppLayout>;
+  return (
+    <AppLayout>
+      <MbtiHeader />
+    </AppLayout>
+  );
 };
 
 export default Profile;
