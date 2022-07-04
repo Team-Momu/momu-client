b1a491d9630571d171f6cbc447ab2ff5eb28f6bf
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-04 17:34:07 +0900

    [feat] footer 완료

--- src/components/MbtiFooter.tsx
+++ src/components/MbtiFooter.tsx
@@ -1,21 +1,45 @@
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 const MbtiFooter = () => {
+  const router = useRouter();
+  const stageNumber: number = Number(router.asPath.split('/')[2]);
+
+  const pushNextPage = (): void => {
+    const pageNumber = stageNumber + 1;
+    router.push(`/profile/${pageNumber}`);
+  };
+  const pushPreviousPage = (): void => {
+    const pageNumber = stageNumber - 1;
+    router.push(`/profile/${pageNumber}`);
+  };
+
   return (
     <>
-      <NextButton>
-        <NextButtonText>다음</NextButtonText>
-      </NextButton>
+      {stageNumber === 1 ? (
+        <NextButton onClick={pushNextPage}>
+          <NextButtonText>다음으로</NextButtonText>
+        </NextButton>
+      ) : (
+        <>
+          <PrevisouButton onClick={pushPreviousPage}>
+            <PreviousButtonText>이전으로</PreviousButtonText>
+          </PrevisouButton>
+          <SmallNextButton onClick={pushNextPage}>
+            <SmallNextButtonText>다음으로</SmallNextButtonText>
+          </SmallNextButton>
+        </>
+      )}
     </>
   );
 };
 
-const NextButton = styled.div`
+const NextButton = styled.button`
   position: absolute;
   width: 343px;
-  height: 56px;
+  height: 44px;
   left: 16px;
-  top: 656px;
+  top: 553px;
 
   display: flex;
   justify-content: center;
@@ -30,13 +54,55 @@ const NextButtonText = styled.div`
   font-weight: 700;
   font-size: 20px;
   line-height: 20px;
-  color: var(--);
+  color: #ffffff;
 `;
 
-const PrevisouButton = styled.div``;
-const PreviousButtonText = styled.div``;
+const PrevisouButton = styled.button`
+  position: absolute;
+  width: 100px;
+  height: 44px;
+  left: 16px;
+  top: 553px;
 
-const SmallNextButton = styled.div``;
-const SmallNextButtonText = styled.div``;
+  background: #5f5f5f;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const PreviousButtonText = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  /* identical to box height, or 125% */
+
+  color: #ffffff;
+`;
+
+const SmallNextButton = styled.button`
+  position: absolute;
+  width: 231px;
+  height: 44px;
+  left: 128px;
+  top: 553px;
+
+  background: #f57906;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SmallNextButtonText = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  /* identical to box height, or 125% */
+
+  color: #ffffff;
+`;
 
 export default MbtiFooter;
