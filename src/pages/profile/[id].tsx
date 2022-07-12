dea8330449a3882e8cb022bc0c017686ffb0ad6f
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-12 21:00:03 +0900

    [chore] mbtiStage 컴포넌트 분리

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,8 +1,16 @@
 import MbtiBody from '@mbti/MbtiBody';
 import MbtiFooter from '@mbti/MbtiFooter';
 import MbtiHeader from '@mbti/MbtiHeader';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
 
 const Profile = () => {
+  const mbti = useSelector((state: RootState) => state.mbti.mbti);
+
+  useEffect(() => {
+    console.log(mbti);
+  }, [mbti]);
   return (
     <>
       <MbtiHeader />
