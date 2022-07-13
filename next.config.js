2f3a80cd2933c8fc1d33a6519c2b82e1bc82e52f
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-13 20:12:17 +0900

    [feat] kakao-login front 90%

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+  // env 내용 추가
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    KAKAO_REDIRECT_URI: process.env.KAKAO_REDIRECT_URI,
+    KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY,
+  },
+};
