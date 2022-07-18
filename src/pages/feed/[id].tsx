9f628a60c01f57fadf84fbc544183bc11eae5cc1
chaaerim <cofla411@ewhain.net>
2022-07-19 04:00:57 +0900

    [feat] url 공유 버튼 구현 완료

--- src/pages/feed/[id].tsx
+++ src/pages/feed/[id].tsx
@@ -18,11 +18,36 @@ const DetailFeed = () => {
     <Wrapper>
       <DetailFeedHeader />
       <DetailFeedContents postId={postId} />
-
-      <button onClick={onClick}>add curation button</button>
+      <>
+        <h1>큐레이션 답변카드</h1>
+        <InputBox type="text" />
+        <AddCurationButton onClick={onClick}>큐레이션 받기</AddCurationButton>
+      </>
     </Wrapper>
   );
 };
 export default DetailFeed;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 375px;
+`;
+
+const InputBox = styled.input`
+  width: 340px;
+  margin-bottom: 200px;
+`;
+
+const AddCurationButton = styled.button`
+  width: 340px;
+  height: 72px;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  /* identical to box height */
+
+  color: #ffffff;
+
+  background-color: #f57a08;
+`;
