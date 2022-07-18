64aa166dd5c5f428550b5d842c90f0481c6b80b6
chaaerim <cofla411@ewhain.net>
2022-07-19 02:55:03 +0900

    [feat] detail curation page 해당 카드 불러오기

--- src/pages/feed/[id].tsx
+++ src/pages/feed/[id].tsx
@@ -1,16 +1,24 @@
-import DetailFeedHeader from 'components/detailfeed/DetailFeedHeader';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
+import DetailFeedContents from 'components/detailfeed/DetailFeed';
+import DetailFeedHeader from 'components/detailfeed/DetailFeedHeader';
 
 const DetailFeed = () => {
   const router = useRouter();
   const onClick = useCallback(() => {
     router.push(`/feed/add`);
   }, []);
+
+  const postId = router.query.id;
+
+  console.log(postId);
+
   return (
     <Wrapper>
       <DetailFeedHeader />
+      <DetailFeedContents postId={postId} />
+
       <button onClick={onClick}>add curation button</button>
     </Wrapper>
   );
--- src/utils/interfaces/curation/curationInterfaces.tsx
+++ src/utils/interfaces/curation/curationInterfaces.tsx
@@ -20,6 +20,33 @@ interface ICurationPost {
   scrap_flag: boolean; //스크랩 여부
 }
 
+interface IDetailCurationPost {
+  message: string;
+  data: {
+    id: number; // 요청 id
+    user: {
+      // 요청 작성한 유저 정보
+      id: number; // 유저 id
+      nickname: string;
+      profile_img: string;
+      mbti: string;
+      level: number;
+      select_count: number;
+    };
+    created_at: string; // 작성일
+    location: string; //장소
+    time: string; //시간대
+    drink: number; //음주 정도
+    member_count: number; //인원수
+    comment_count: number; //답글 수
+    description: string; //설명
+    selected_flag: boolean;
+    scrap_flag: boolean; //스크랩 여부
+    comments: string[];
+  };
+  pending: boolean;
+}
+
 interface ICurationPostLists {
   message: string;
   data: {
@@ -30,4 +57,4 @@ interface ICurationPostLists {
   pending: boolean;
 }
 
-export type { ICurationPost, ICurationPostLists };
+export type { ICurationPost, ICurationPostLists, IDetailCurationPost };
