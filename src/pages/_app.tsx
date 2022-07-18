aa2168de297a25419f9db1324e0d244ac7a3f671
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-19 00:28:54 +0900

    [feat] axios set

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,10 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/FilterStyle.css';
 import axios from 'axios';
-import { ToastContainer } from 'react-toastify';
+import { instance } from '../lib/axios';
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
-axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+// axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
