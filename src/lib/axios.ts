7868979f58a266bf50c2aa3991e7de6388930145
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-19 19:18:17 +0900

    [feat] axios set

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
-  // headers: {
-  //   'Access-Control-Allow-Origin': 'https://api.momueat.com', // 서버 domain
-  // },
+  headers: {
+    'Access-Control-Allow-Origin': 'https://api.momueat.com', // 서버 domain
+  },
 });
