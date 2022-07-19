c4c891a38720701a611fa7f2f87ad98874481dc3
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-07-20 00:34:22 +0900

    [chore] axios setting

--- src/store/slices/user/userThunk.tsx
+++ src/store/slices/user/userThunk.tsx
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { instance } from '../../../lib/axios';
+import axios from 'axios';
 export const kakao = createAsyncThunk(
   'user/kakao',
   async (code: string, thunkAPI) => {
-    const response = await instance.get(`/user/kakao/authorize/?code=${code}`);
+    const response = await axios.get(`/user/kakao/authorize/?code=${code}`);
     return response.data;
   }
 );
