7c20810c145fbfa3d141dff4153e763414633685
chaaerim <cofla411@ewhain.net>
2022-07-21 13:48:15 +0900

    [feat] add comment layout 잡기

--- src/components/comment/AddComment.tsx
+++ src/components/comment/AddComment.tsx
@@ -28,13 +28,12 @@ const AddComment = () => {
       e.preventDefault();
       setWhereToGo('');
       setWhere(whereToGo);
+
+      setIsOpen(true);
     },
     [whereToGo]
   );
 
-  function openModal() {
-    setIsOpen(true);
-  }
   function closeModal() {
     setIsOpen(false);
   }
@@ -47,15 +46,27 @@ const AddComment = () => {
   console.log(placeDatas);
 
   return (
-    <>
-      <form onSubmit={onSubmitPlace}>
-        <PlaceInput
-          type="text"
-          onChange={handleInputChange}
-          placeholder="식당검색. . ."
-        ></PlaceInput>
-      </form>
-      <button onClick={openModal}>식당검색</button>
+    <Wrapper>
+      <SearchPlace>
+        <GuideText>큐레이션 식당 검색</GuideText>
+        <form onSubmit={onSubmitPlace}>
+          <PlaceInput
+            type="text"
+            onChange={handleInputChange}
+            placeholder="원하는 식당을 검색해주세요!"
+          ></PlaceInput>
+        </form>
+      </SearchPlace>
+      <InnerContainer>
+        <GuideText>사진 (선택)</GuideText>
+        <ImgUploadButton></ImgUploadButton>
+      </InnerContainer>
+      <InnerContainer>
+        <GuideText>큐레이션 작성</GuideText>
+        <CommentTextInput
+          placeholder="자세하게 적어줄 수록 채택확률이 높아요! &#10;(최대 38자)"
+        ></CommentTextInput>
+      </InnerContainer>
 
       <div style={{ position: 'relative' }}>
         <Modal
@@ -80,13 +91,72 @@ const AddComment = () => {
           <div>I am a modal</div>
         </Modal>
       </div>
-    </>
+    </Wrapper>
   );
 };
 export default AddComment;
+const Wrapper = styled.div`
+  width: 375px;
+`;
 
 const PlaceInput = styled.input`
-  width: 200px;
+  width: 343px;
   height: 42px;
   border: 1px solid #191919;
+  background-image: url('/img/search/searchIcon.svg');
+  background-repeat: no-repeat;
+  background-position: 17px center;
+  &:placeholder-shown {
+    padding-left: 45px;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    /* identical to box height, or 143% */
+
+    color: #767676;
+  }
+`;
+
+const SearchPlace = styled.div`
+  padding-top: 36px;
+`;
+const GuideText = styled.div`
+  margin-bottom: 14px;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+  /* identical to box height */
+
+  color: #191919;
+`;
+
+const InnerContainer = styled.div`
+  margin-top: 40px;
+`;
+
+const ImgUploadButton = styled.button`
+  width: 343px;
+  height: 206px;
+  background: #ededed;
+`;
+
+const CommentTextInput = styled.textarea`
+  width: 343px;
+  height: 80px;
+  border: 1px solid #191919;
+  resize: none;
+  &:placeholder-shown {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 30px;
+    /* identical to box height, or 143% */
+
+    color: #767676;
+  }
 `;
