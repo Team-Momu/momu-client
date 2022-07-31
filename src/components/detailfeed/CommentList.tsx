fce331a0cd4e3e291114005208b0f5bd08f930b8
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-08-01 04:46:17 +0900

    deploy

--- src/components/detailfeed/CommentList.tsx
+++ src/components/detailfeed/CommentList.tsx
@@ -31,22 +31,22 @@ const CommentList: FC<Props> = ({ postId }) => {
       {commentLists.map((comment) => {
         return (
           <>
-            {/*<CommentCard*/}
-            {/*  key={comment.id + `${new Date()}`}*/}
-            {/*  userId={comment.post_user}*/}
-            {/*  postId={postId}*/}
-            {/*  commentId={comment.id}*/}
-            {/*  selectedFlag={comment.select_flag}*/}
-            {/*  placeImg={comment.place_img}*/}
-            {/*  writerName={comment.user.nickname}*/}
-            {/*  writerProfile={comment.user.profile_img}*/}
-            {/*  writerMbti={comment.user.mbti}*/}
-            {/*  description={comment.description}*/}
-            {/*  placeName={comment.place.place_name}*/}
-            {/*  placeAddress={comment.place.road_address_name}*/}
-            {/*  placeCategory={comment.place.category_name}*/}
-            {/*  createAt={comment.created_at}*/}
-            {/*/>*/}
+            <CommentCard
+              key={comment.id + `${new Date()}`}
+              userId={comment.post_user}
+              postId={postId}
+              commentId={comment.id}
+              selectedFlag={comment.select_flag}
+              placeImg={comment.place_img}
+              writerName={comment.user.nickname}
+              writerProfile={comment.user.profile_img}
+              writerMbti={comment.user.mbti}
+              description={comment.description}
+              placeName={comment.place.place_name}
+              placeAddress={comment.place.road_address_name}
+              placeCategory={comment.place.category_name}
+              createAt={comment.created_at}
+            />
           </>
         );
       })}
